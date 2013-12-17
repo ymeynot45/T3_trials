0109bb415e79e429e1ac1d51282ed01f64c0015d
William Karsten <yme_ynot45@hotmail.com>
2013-12-17 15:12:05 -0600

    planning and json setup

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,19 @@
 get '/' do
   erb :index
 end
+
+post '/playermove' do
+  #take in player's move
+  #process player move
+  #Evaluate if the player won the game
+  #AI places a piece
+  #Evaluate if the AI won the game
+  #Insert moves back to the page using json
+  content_type :json
+  {
+    playermove: "#{}",
+    aimove: "#{}",
+    winner: ""
+  }.to_json
+
+end
