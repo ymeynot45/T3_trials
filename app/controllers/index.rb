0cef9777524a5bc47e4340d565c3ceff2de413e7
William Karsten <yme_ynot45@hotmail.com>
2014-02-26 18:17:59 -0600

    illegal move bypass

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,12 +6,16 @@ end
 post '/playermove' do
   move = params[:move]
   currentboard = params[:board]
-  currentboard.gsub!("#{move}", "X")
-  #process player move (evaluate if move was legal)
-  #Evaluate if the player won the game
-  #AI places a piece
-  #Evaluate if the AI won the game
-  #Insert moves back to the page using json
+  if currentboard.include? "#{move}"
+    currentboard.gsub!("#{move}", "X")
+    #process player move (evaluate if move was legal)
+    #Evaluate if the player won the game
+    #AI places a piece
+    #Evaluate if the AI won the game
+    #Insert moves back to the page using json
+  else
+    #Possible put an error message here for picking a taken box.
+  end
   if request.xhr?
     content_type :json
     {playermove: params[:move],
