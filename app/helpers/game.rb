175f16a1cf4f71ad4e5664f52952757b4e1a6eb9
William Karsten <yme_ynot45@hotmail.com>
2014-03-02 20:12:44 -0600

    fixing last commit w/out squash to show workflow

--- app/helpers/game.rb
+++ app/helpers/game.rb
@@ -38,7 +38,7 @@ end
 def check_rows(board)
   rows = board.scan(/.../)
   move = line_check(rows, "O")
-  if move && move =~ /d/
+  if move
     return move
   end
   false
@@ -51,7 +51,7 @@ def check_col(board)
   thirdcol = board.values_at(2, 5, 8)
   cols = [firstcol.join, secondcol.join, thirdcol.join]
   move = line_check(cols, "O")
-  if move && move =~ /d/
+  if move
     return move
   end
   false
@@ -63,7 +63,7 @@ def check_diag(board)
   right = board.values_at(2, 4, 6)
   dia = [left.join, right.join]
   move = line_check(dia, "O")
-  if move && move =~ /d/
+  if move
     return move
   end
   false
@@ -73,7 +73,7 @@ end
 def block_rows(board)
   rows = board.scan(/.../)
   move = line_check(rows, "X")
-  if move && move =~ /d/
+  if move
     return move
   end
   false
@@ -86,7 +86,7 @@ def block_col(board)
   thirdcol = board.values_at(2, 5, 8)
   cols = [firstcol.join, secondcol.join, thirdcol.join]
   move = line_check(cols, "X")
-  if move && move =~ /d/
+  if move
     return move
   end
   false
@@ -102,7 +102,7 @@ def block_diag(board)
   right = board.values_at(2, 4, 6)
   dia = [left.join, right.join]
   move = line_check(dia, "X")
-  if move && move =~ /d/
+  if move
     return move
   end
   false
@@ -112,7 +112,9 @@ def line_check(line, piece)
   line.each{ |line|
     if line.count(piece) == 2
       line.gsub!(piece, "")
-      return line
+      if line != "X" and line != "O"
+        return line
+      end
     end 
   }
   false
