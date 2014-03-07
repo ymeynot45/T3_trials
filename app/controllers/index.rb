c6953d9a412a35de92f1c29dc6b7d2574b15c982
William Karsten <yme_ynot45@hotmail.com>
2014-03-07 13:36:08 -0600

    moved the ruby into a class

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,37 +4,18 @@ get '/' do
 end
 
 post '/playermove' do
-  move = params[:move]
-  currentboard = params[:board]
-  message = ""
-  cpumove = "0"
-  if currentboard.include? "#{move}"
-    currentboard.gsub!("#{move}", "X")
-    if player_win?(currentboard)
-      message = "YOU WIN!"
-    elsif currentboard.count("X") == 5
-      message = "IT'S A TIE"
-    else
-      cpumove = cpu_move(currentboard)
-      currentboard.gsub!("#{cpumove}", "O")
-      if cpu_win?(currentboard)
-      message = "YOU LOOSE!"
-      end
-    end
-  else
-    message = "Invalid Move"
-    move = "0"
-  end
+  currentgame = Game.new(params[:board], params[:move])
+  currentgame.process
   if request.xhr?
     content_type :json
-    {playermove: "#{move}",
+    {playermove: "#{currentgame.move}",
     playervalue: "X",
     playercolor: "blue",
-    cpumove: "#{cpumove}",
+    cpumove: "#{currentgame.cpumove}",
     cpuvalue: "O",
     cpucolor: "red",
-    currentboard: "#{currentboard}",
-    message: message}.to_json
+    currentboard: "#{currentgame.currentboard}",
+    message: "#{currentgame.message}"}.to_json
   else
     #if JS is turned off we can run it all using the currentboard string. NOT implemented yet.
     erb :index
--- app/helpers/game.rb
+++ app/helpers/game.rb
@@ -1,159 +1,194 @@
-def player_win?(board)
-playerwin = [/XXX....../, /...XXX.../, /......XXX/, /X..X..X../, /.X..X..X./, /..X..X..X/, /X...X...X/, /..X.X.X../]
-  if playerwin.any? { |win| board =~ win }
-    return true
+class Game
+  attr_reader(:currentboard, :move, :message, :cpumove)
+  
+  def initialize(board, move)
+    @currentboard = board
+    @move = move
+    @message = ""
+    @cpumove = "0"
   end
-  false 
-end
 
-def cpu_win?(board)
-  cpuwin = [/OOO....../, /...OOO.../, /......OOO/, /O..O..O../, /.O..O..O./, /..O..O..O/, /O...O...O/, /..O.O.O../]
-  if cpuwin.any? { |win| board =~ win }
-    return true
-  end
-   false
-end
-
-def cpu_move(board)
-  if board.include? "5"
-    return "5"
-  elsif move = check_rows(board)
-    return move
-  elsif move = check_col(board)
-    return move
-  elsif move = check_diag(board)
-    return move
-  elsif move = block_rows(board)
-    return move
-  elsif move = block_col(board)
-    return move
-  elsif move = block_diag(board)
-    return move
-  elsif board[6] == "7" and seven_block(board)
-    return "7"
-  elsif board[8] == "9" and nine_block(board)
-    return "9"
-  elsif board.include? "1" and board.include? "3" and board.include? "7" and board.include? "9"
-    return "3"
-  elsif board[4] == "O" and forking_block(board)
-    return "2"
-  else
-    return random_move(board)
-  end
-end
-
-def check_rows(board)
-  rows = board.scan(/.../)
-  move = line_check(rows, "O")
-  if move
-    return move
-  end
-  false
-end
-
-def check_col(board)
-  board = board.split(//)
-  firstcol = board.values_at(0, 3, 6)
-  secondcol = board.values_at(1, 4, 7)
-  thirdcol = board.values_at(2, 5, 8)
-  cols = [firstcol.join, secondcol.join, thirdcol.join]
-  move = line_check(cols, "O")
-  if move
-    return move
-  end
-  false
-end
-
-def check_diag(board)
-  board = board.split(//)
-  left = board.values_at(0, 4, 8)
-  right = board.values_at(2, 4, 6)
-  dia = [left.join, right.join]
-  move = line_check(dia, "O")
-  if move
-    return move
-  end
-  false
-end
-
-def block_rows(board)
-  rows = board.scan(/.../)
-  move = line_check(rows, "X")
-  if move
-    return move
-  end
-  false
-end
-
-def block_col(board)
-  board = board.split(//)
-  firstcol = board.values_at(0, 3, 6)
-  secondcol = board.values_at(1, 4, 7)
-  thirdcol = board.values_at(2, 5, 8)
-  cols = [firstcol.join, secondcol.join, thirdcol.join]
-  move = line_check(cols, "X")
-  if move
-    return move
-  end
-  false
-end
-
-def block_diag(board)
-  if board[4] == "O"
-    return false
-  end
-  board = board.split(//)
-  left = board.values_at(0, 4, 8)
-  right = board.values_at(2, 4, 6)
-  dia = [left.join, right.join]
-  move = line_check(dia, "X")
-  if move
-    return move
-  end
-  false
-end
-
-def line_check(line, piece)
-  line.each{ |line|
-    if line.count(piece) == 2
-      line.gsub!(piece, "")
-      if line != "X" and line != "O"
-        return line
+  def process
+    if @currentboard.include? "#{@move}"
+      @currentboard.gsub!("#{@move}", "X")
+      if player_win?(@currentboard)
+        change_message("YOU WIN!")
+      elsif @currentboard.count("X") == 5
+        change_message("IT'S A TIE")
+      else
+        @cpumove = cpu_move(@currentboard)
+        @currentboard.gsub!("#{@cpumove}", "O")
+        if cpu_win?(@currentboard)
+          change_message("YOU LOOSE!")
+        end
       end
-    end 
-  }
-  false
-end
-
-def seven_block(board)
-  if board[0] == "X" and board[7] == "X"
-    return true
-  elsif board[3] == "X" and board[7] == "X"
-    return true
+    else
+      change_message("Invalid Move")
+      @move = "0"
+    end
   end
-  false
-end
 
-def nine_block(board)
-  if board[2] == "X" and board[7] == "X"
-    return true
-  elsif board[5] == "X" and board[6] == "X"
-    return true
+  def player_win?(board)
+  playerwin = [/XXX....../, /...XXX.../, /......XXX/, /X..X..X../, /.X..X..X./, /..X..X..X/, /X...X...X/, /..X.X.X../]
+    if playerwin.any? { |win| board =~ win }
+      return true
+    end
+    false 
   end
-  false
-end
 
-def forking_block(board)
-  if board[0] == "X" and board[8] == "X"
-    return true
-  elsif board[2] == "X" and board[6] == "X"
-    return true
-  else
+  def cpu_win?(board)
+    cpuwin = [/OOO....../, /...OOO.../, /......OOO/, /O..O..O../, /.O..O..O./, /..O..O..O/, /O...O...O/, /..O.O.O../]
+    if cpuwin.any? { |win| board =~ win }
+      return true
+    end
+     false
+  end
+
+  def cpu_move(board)
+    if board.include? "5"
+      return "5"
+    elsif move = check_rows(board)
+      return move
+    elsif move = check_col(board)
+      return move
+    elsif move = check_diag(board)
+      return move
+    elsif move = block_rows(board)
+      return move
+    elsif move = block_col(board)
+      return move
+    elsif move = block_diag(board)
+      return move
+    elsif board[6] == "7" and seven_block(board)
+      return "7"
+    elsif board[8] == "9" and nine_block(board)
+      return "9"
+    elsif board.include? "1" and board.include? "3" and board.include? "7" and board.include? "9"
+      return "3"
+    elsif board[4] == "O" and forking_block(board)
+      return "2"
+    else
+      return random_move(board)
+    end
+  end
+
+  def check_rows(board)
+    rows = board.scan(/.../)
+    move = line_check(rows, "O")
+    if move
+      return move
+    end
     false
   end
-end
 
-def random_move(board)
-  move = board.gsub(/[OX]/, "")
-  return move[/(.)/]
+  def check_col(board)
+    board = board.split(//)
+    firstcol = board.values_at(0, 3, 6)
+    secondcol = board.values_at(1, 4, 7)
+    thirdcol = board.values_at(2, 5, 8)
+    cols = [firstcol.join, secondcol.join, thirdcol.join]
+    move = line_check(cols, "O")
+    if move
+      return move
+    end
+    false
+  end
+
+  def check_diag(board)
+    board = board.split(//)
+    left = board.values_at(0, 4, 8)
+    right = board.values_at(2, 4, 6)
+    dia = [left.join, right.join]
+    move = line_check(dia, "O")
+    if move
+      return move
+    end
+    false
+  end
+
+  def block_rows(board)
+    rows = board.scan(/.../)
+    move = line_check(rows, "X")
+    if move
+      return move
+    end
+    false
+  end
+
+  def block_col(board)
+    board = board.split(//)
+    firstcol = board.values_at(0, 3, 6)
+    secondcol = board.values_at(1, 4, 7)
+    thirdcol = board.values_at(2, 5, 8)
+    cols = [firstcol.join, secondcol.join, thirdcol.join]
+    move = line_check(cols, "X")
+    if move
+      return move
+    end
+    false
+  end
+
+  def block_diag(board)
+    if board[4] == "O"
+      return false
+    end
+    board = board.split(//)
+    left = board.values_at(0, 4, 8)
+    right = board.values_at(2, 4, 6)
+    dia = [left.join, right.join]
+    move = line_check(dia, "X")
+    if move
+      return move
+    end
+    false
+  end
+
+  def line_check(line, piece)
+    line.each{ |line|
+      if line.count(piece) == 2
+        line.gsub!(piece, "")
+        if line != "X" and line != "O"
+          return line
+        end
+      end 
+    }
+    false
+  end
+
+  def seven_block(board)
+    if board[0] == "X" and board[7] == "X"
+      return true
+    elsif board[3] == "X" and board[7] == "X"
+      return true
+    end
+    false
+  end
+
+  def nine_block(board)
+    if board[2] == "X" and board[7] == "X"
+      return true
+    elsif board[5] == "X" and board[6] == "X"
+      return true
+    end
+    false
+  end
+
+  def forking_block(board)
+    if board[0] == "X" and board[8] == "X"
+      return true
+    elsif board[2] == "X" and board[6] == "X"
+      return true
+    else
+      false
+    end
+  end
+
+  def random_move(board)
+    move = board.gsub(/[OX]/, "")
+    return move[/(.)/]
+  end
+
+  def change_message(message)
+    @message = message
+  end
 end
